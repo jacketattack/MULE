2401a646042802c3e5ca469b9fc4779400e27e7b
stronglink <grant@granttitus.com>
2013-09-28 09:49:23 -0400

    created main jframe

---
+++ src/main/java/gui/Window.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class Window extends JFrame 
+{
+	public Window ()
+	{
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+	}
+}
