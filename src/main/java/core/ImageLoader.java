acaef982b2e3d1be87539d65ca5b7397c55d1ea2
stronglink <grant@granttitus.com>
2013-10-07 23:22:37 -0400

    added image caching (no size limit)

--- src/main/java/core/ImageLoader.java
+++ src/main/java/core/ImageLoader.java
@@ -3,25 +3,39 @@ package core;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
 public class ImageLoader 
 {
+	// cache
+	private HashMap<String, Image> hashMap;
+	
 	private static ImageLoader instance;
 	
 	private ImageLoader()
 	{
-		
+		hashMap = new HashMap<String, Image>();
 	}
 	
 	public Image load(String path)
 	{
 		Image image = null;
 		
+		if (hashMap.containsKey(path))
+		{
+			return hashMap.get(path);
+		}
+		
 		try 
 		{
 			image = ImageIO.read(new File(path));
+			
+			if (!hashMap.containsKey(path))
+			{
+				hashMap.put(path, image);
+			}
 		} 
 		catch (IOException e)
 		{
