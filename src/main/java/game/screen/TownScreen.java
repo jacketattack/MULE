1d8377d6445ab1e81101350cdbcb75058f1474d8
Nick Teissler <nick.teissler@gmail.com>
2013-10-16 11:18:04 -0400

    fitted image size and layout

--- src/main/java/game/screen/TownScreen.java
+++ src/main/java/game/screen/TownScreen.java
@@ -16,43 +16,43 @@ public class TownScreen extends Screen
 		renderableStrings.clear();
 
 		SimpleRender energyStore = new SimpleRender("assets/images/store/storeEnergy.png");
-		energyStore.setX(534);
+		energyStore.setX(496);
 		energyStore.setY(0);
 		renderables.add(energyStore);
 
 		SimpleRender foodStore = new SimpleRender("assets/images/store/storeFood.png");
-		foodStore.setX(9);
+		foodStore.setX(138);
 		foodStore.setY(0);
 		renderables.add(foodStore);
 
 		SimpleRender oreStore = new SimpleRender("assets/images/store/storeOre.png");
-		oreStore.setX(438);
+		oreStore.setX(362);
 		oreStore.setY(0);
 		renderables.add(oreStore);
 
 		SimpleRender crystite = new SimpleRender("assets/images/store/storeCrystite.png");
-		crystite.setX(0);
+		crystite.setX(4);
 		crystite.setY(0);
 		renderables.add(crystite);
 		
 		SimpleRender muleStore = new SimpleRender("assets/images/store/storeMule.png");
-		muleStore.setX(534);
-		muleStore.setY(230);
+		muleStore.setX(496);
+		muleStore.setY(200);
 		renderables.add(muleStore);
 
 		SimpleRender landStore = new SimpleRender("assets/images/store/storeLand.png");
-		landStore.setX(96);
-		landStore.setY(230);
+		landStore.setX(138);
+		landStore.setY(200);
 		renderables.add(landStore);
 
 		SimpleRender pub = new SimpleRender("assets/images/store/storePub.png");
-		pub.setX(438);
-		pub.setY(230);
+		pub.setX(362);
+		pub.setY(200);
 		renderables.add(pub);
 
 		SimpleRender assay = new SimpleRender("assets/images/store/storeAssay.png");
-		assay.setX(0);
-		assay.setY(230);
+		assay.setX(4);
+		assay.setY(200);
 		renderables.add(assay);
 		
 	}
