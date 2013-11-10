51bae97ed69263a99eaf5e203b73c6540c477896
jacketattack <trvrmay@yahoo.com>
2013-11-10 13:50:05 -0800

    initial commit with skeletal classes for round and screen

---
+++ src/main/java/game/round/ProductionRound.java
@@ -0,0 +1,29 @@
+package game.round;
+
+public class ProductionRound extends Round {
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void click(int x, int y, boolean leftMouse) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/game/screen/ProductionScreen.java
@@ -0,0 +1,17 @@
+package game.screen;
+
+public class ProductionScreen extends Screen {
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean shouldSwitch() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
