38d97f9ca82d7a9bbc77aec8a102b5e08952861b
jacketattack <trvrmay@yahoo.com>
2013-12-01 18:39:29 -0800

    fail at git I did.now committing all stuff as described before

--- src/main/java/game/state/GameFinishState.java
+++ src/main/java/game/state/GameFinishState.java
@@ -1,11 +1,18 @@
 package game.state;
 
+import game.Session;
+
 /**
  * implementation to come...
  * @author grant
  */
 public class GameFinishState implements State 
 {
+	
+	public GameFinishState(Session session)
+	{
+		
+	}
 	/**
 	 * implementation to come...
 	 */
