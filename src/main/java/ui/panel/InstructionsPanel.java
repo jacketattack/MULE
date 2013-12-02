bd294c7d297c02c1760060216e93de484acb0a07
Nick Teissler <nick.teissler@gmail.com>
2013-12-02 11:12:15 -0500

    need to type instructions and make background image work, and instructions image, then done

--- src/main/java/ui/panel/InstructionsPanel.java
+++ src/main/java/ui/panel/InstructionsPanel.java
@@ -1,7 +1,14 @@
 package ui.panel;
 
+import core.Callable;
+import core.StateSelector;
+import game.state.MenuState;
+import ui.Window;
+import ui.render.Render;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -13,19 +20,24 @@ import java.util.Scanner;
  * Time: 1:06 PM
  * To change this template use File | Settings | File Templates.
  */
-public class InstructionsPanel extends JPanel
+public class InstructionsPanel extends RenderPanel
 {
     private JLabel title;
     private JTextArea text;
     private JScrollPane scroller;
     private Scanner instructionsStream;
+    private Render backgroundRender;
 
     public InstructionsPanel()
     {
+
+        backgroundRender = new Render();
+        backgroundRender.addImage("assets/images/background.png");
+        renders.add(backgroundRender);
         loadText();
         this.setLayout(new BorderLayout(0, 5));
         title = new JLabel("Instructions");
-        text = new JTextArea(20,50);
+        text = new JTextArea();
         text.setEditable(false);
         text.setText("");
 
@@ -35,17 +47,39 @@ public class InstructionsPanel extends JPanel
 
         while (instructionsStream.hasNextLine())
         {
-            text.append(instructionsStream.nextLine());
+            text.append(instructionsStream.nextLine()+"\n");
         }
 
         scroller = new JScrollPane(text);
         scroller.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-
-
+        text.setCaretPosition(0);
 
         //add everything to the layout below here
+        this.setBorder(BorderFactory.createEmptyBorder(5,9,70,9));
         add(title, BorderLayout.NORTH);
         add(scroller,BorderLayout.CENTER);
+
+        ui.Button backButton = new ui.Button("assets/images/buttons/backDefault.png", "assets/images/buttons/backHover.png", "assets/images/buttons/backClick.png");
+        backButton.setWidth(71);
+        backButton.setHeight(33);
+        backButton.setX(539);
+        backButton.setY(367);
+        onHover(backButton, backButton.HOVER_COMMAND, backButton.UNHOVER_COMMAND);
+        onPress(backButton, backButton.PRESS_COMMAND);
+        onRelease(backButton, new Callable()
+        {
+            public void call()
+            {
+                Window window = Window.getInstance();
+                window.setPanel(new MenuPanel());
+
+                StateSelector stateSelector = StateSelector.getInstance();
+                stateSelector.setState(new MenuState());
+
+            }
+        });
+        buttons.add(backButton);
+
 
    }
 
@@ -57,6 +91,7 @@ public class InstructionsPanel extends JPanel
             System.out.println("File not found in instructions");
         }
     }
+
 
 
 }
